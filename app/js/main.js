01a3555677e70fdbe1349c07f862b7b157843977
Marcin Kumorek <Marcin.Kumorek@Gamesys.co.uk>
2013-11-25 15:05:39 +0000

    Add lodash to main.js

--- app/js/main.js
+++ app/js/main.js
@@ -2,11 +2,18 @@ requirejs.config({
 	baseUrl: 'app',
 	paths: {
 		'jquery': '../components/jquery/jquery'
-	}
+	},
+	packages: [
+		{ name: 'lodash', location: '../components/lodash', main: 'lodash' }
+	]
 })
 
-require(['jquery'], function ($) {
+require(['jquery', 'lodash'], function ($, _) {
 	$(function () {
 		$('h1').text('This is jQuery - loaded from main.js')
+		var uppers = _(['one', 'two', 'three']).map(function (str) {
+			return str.toUpperCase()
+		})
+		console.log(uppers)
 	})
 })
