a6a58c90745d5e62a82736cce69157725182168e
Marcin Kumorek <Marcin.Kumorek@Gamesys.co.uk>
2013-11-25 14:43:27 +0000

    Add jQuery and normalize.css

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
-require([], function () {
-	console.log('main.js')
+requirejs.config({
+	baseUrl: 'app',
+	paths: {
+		'jquery': '../components/jquery/jquery'
+	}
+})
+
+require(['jquery'], function ($) {
+	$(function () {
+		console.log('This is jQuery - loaded from main.js')
+	})
 })
