f0d4cb2d620f122b8fd29531ceca6ccca96750b8
Marcin Kumorek <Marcin.Kumorek@Gamesys.co.uk>
2013-11-26 17:24:38 +0000

    Update paths, files, deps and callback for tests

--- test/test-main.js
+++ test/test-main.js
@@ -12,22 +12,44 @@ requirejs.config({
 	baseUrl: '/base/app',
 
 	paths: {
-		'jquery': '../components/jquery/jquery'
+		'jquery': 'components/jquery/jquery',
+		'chai': 'components/chai/chai',
+		// 'chai': '../node_modules/chai/chai',
+		'sinon': 'components/sinon/lib/sinon',
+		'sinon-chai': 'components/sinon-chai/lib/sinon-chai'
+
+		// 'underscore': '../components/underscore/underscore',
+		// 'backbone': '../components/backbone-amd/backbone',
+		// 'marionette': '../components/marionette/lib/backbone.marionette',
 	},
 
 	packages: [
-		{ name: 'lodash', location: '../components/lodash', main: 'lodash' }
+		{ name: 'lodash', location: 'components/lodash', main: 'lodash' }
 	],
 
 	// shim: {
 	// 	'underscore': {
 	// 		exports: '_'
+	// 	},
+	// 	'sinon': {
+	// 		exports: 'sinon'
+	// 	},
+	// 	'marionette': {
+	// 		exports: 'Marionette',
+	// 		deps: ['backbone']
 	// 	}
 	// },
 
 	// ask Require.js to load these files (all our tests)
-	deps: tests,
+	deps: ['chai', 'sinon', 'sinon-chai'].concat(tests),
 
 	// start test run, once Require.js is done
-	callback: window.__karma__.start
+	callback: function(chai, sinon, sinonChai) {
+		window.expect = chai.expect;
+		window.sinon = sinon;
+
+		chai.use(sinonChai)
+
+		window.__karma__.start()
+	}
 });
