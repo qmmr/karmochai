507f431cbd62297d5da9d94ca4c097aa41d40c21
Marcin Kumorek <Marcin.Kumorek@Gamesys.co.uk>
2013-11-27 12:30:36 +0000

    Update app that returns compact array and test for it

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,4 @@
 define(['lodash'], function (_) {
-	var truthies = _.compact([0, undefined, 1, null, 2, '', 3, false, 4, NaN, 5])
-
-	return {
-		truthyValues: truthies
-	}
+	var mixedArray = [0, 1, '', 2, null, 3, undefined, 4, NaN, 5, false]
+	return _.compact(mixedArray)
 })
--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -1,29 +1,28 @@
-define(['jquery', 'lodash'], function ($, _) {
-	var testArray = [0, 1, '', 2, null, 3, undefined, 4, NaN, 5, false]
+define(['jquery', 'lodash', 'js/app'], function ($, _, mixedArray) {
+	var originalArray = [0, 1, '', 2, null, 3, undefined, 4, NaN, 5, false]
 
 	describe('Falsy values Array test', function() {
-		var truthyValuesArray = _.compact(testArray)
-
-		it('should have 11 values', function() {
-			expect(testArray).to.have.length(11)
-		})
 
 		it('should have only truthy values', function() {
-			expect(truthyValuesArray).to.have.length(5)
-			expect(truthyValuesArray).to.include(1)
-			expect(truthyValuesArray).to.include(2)
-			expect(truthyValuesArray).to.include(3)
-			expect(truthyValuesArray).to.include(4)
-			expect(truthyValuesArray).to.include(5)
+			expect(mixedArray).to.have.length(5)
+			expect(mixedArray).to.include(1)
+			expect(mixedArray).to.include(2)
+			expect(mixedArray).to.include(3)
+			expect(mixedArray).to.include(4)
+			expect(mixedArray).to.include(5)
 		})
 
 		it('should not have "falsy" values', function() {
-			expect(truthyValuesArray).not.to.include(false)
-			expect(truthyValuesArray).not.to.include(NaN)
-			expect(truthyValuesArray).not.to.include(null)
-			expect(truthyValuesArray).not.to.include('')
-			expect(truthyValuesArray).not.to.include(0)
-			expect(truthyValuesArray).not.to.include(undefined)
+			expect(mixedArray).not.to.include(false)
+			expect(mixedArray).not.to.include(NaN)
+			expect(mixedArray).not.to.include(null)
+			expect(mixedArray).not.to.include('')
+			expect(mixedArray).not.to.include(0)
+			expect(mixedArray).not.to.include(undefined)
+		})
+
+		it('should have exact params', function() {
+			expect(mixedArray).to.deep.equal([1,2,3,4,5])
 		})
 	})
 })
