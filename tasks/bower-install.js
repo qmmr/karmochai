1e3a615b34d087c5a2653db3aba6bdc2207d1bb2
Marcin Kumorek <Marcin.Kumorek@Gamesys.co.uk>
2013-11-28 16:15:27 +0000

    Update bower-install grunt task

--- tasks/bower-install.js
+++ tasks/bower-install.js
@@ -1,16 +1,17 @@
 module.exports = function (grunt) {
-	grunt.registerMultitask('bower-install', function () {
+	grunt.registerTask('bower-install', 'Install bower dependencies.', function () {
 		var done = this.async()
 		var bower = require('bower')
+
+		grunt.log.subhead('Initialize bower install...')
+
 		bower.commands.install()
 			.on('log', function (resp) {
-				grunt.log.oklns(resp)
+				grunt.log.oklns(resp.id + ' ' + resp.message)
 			})
-			.on('error', function (resp) {
-				grunt.fail.warn(resp)
-			})
+			.on('error', grunt.fail.warn)
 			.on('end', function (resp) {
-				grunt.subhead()
+				grunt.log.subhead('All bower components have been installed, cheers!')
 				done()
 			})
 	})
